0e1387ff85ca30ff3154d59b96c5fa9c70873981
Mark Berger <mark@mhb.nz>
2021-06-14 15:33:15 +1200

    Integration Tests – Thymeleaf Template Engine.

---
+++ jbake-core/src/test/groovy/org/jbake/template/ThymeleafTemplateEngineRenderingIntegrationTest.groovy
@@ -0,0 +1,8 @@
+package org.jbake.template
+
+class ThymeleafTemplateEngineRenderingIntegrationTest extends org.jbake.template.AbstractTemplateEngineRenderingIntegrationTest {
+
+    public ThymeleafTemplateEngineRenderingIntegrationTest() {
+        super("thymeleafTemplates", "thyme");
+    }
+}
