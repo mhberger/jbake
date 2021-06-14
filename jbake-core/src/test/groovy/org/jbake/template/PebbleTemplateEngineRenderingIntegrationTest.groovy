501ef296804a0fcee777f2350f0f61ad48a02b8a
Mark Berger <mark@mhb.nz>
2021-06-14 15:31:29 +1200

    Integration Tests – Pebble Template Engine.

---
+++ jbake-core/src/test/groovy/org/jbake/template/PebbleTemplateEngineRenderingIntegrationTest.groovy
@@ -0,0 +1,8 @@
+package org.jbake.template
+
+class PebbleTemplateEngineRenderingIntegrationTest extends org.jbake.template.AbstractTemplateEngineRenderingIntegrationTest {
+
+    public PebbleTemplateEngineRenderingIntegrationTest() {
+        super("pebbleTemplates", "pebble");
+    }
+}
