a880c004e015a792cdd25de36add4bc3beabedac
Mark Berger <mark@mhb.nz>
2021-06-14 15:29:00 +1200

    Integration Tests – Jade Template Engine.

---
+++ jbake-core/src/test/groovy/org/jbake/template/JadeTemplateEngineRenderingIntegrationTest.groovy
@@ -0,0 +1,8 @@
+package org.jbake.template
+
+class JadeTemplateEngineRenderingIntegrationTest extends org.jbake.template.AbstractTemplateEngineRenderingIntegrationTest {
+
+    public JadeTemplateEngineRenderingIntegrationTest() {
+        super("jadeTemplates", "jade");
+    }
+}
