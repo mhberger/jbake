c6647e914ce93722b0bdd2924e7dd5459cb85354
Mark Berger <mark.berger@mhb.nz>
2021-06-27 23:40:30 +1200

    EncodingTest - add simple test to identify encoding issue on Windows.
    We can see the file is encoded correctly on storage. So code reading it
    muat be in error.

--- jbake-core/src/test/groovy/org/jbake/configuration/EncodingTest.groovy
+++ jbake-core/src/test/groovy/org/jbake/configuration/EncodingTest.groovy
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.Test
 class EncodingTest {
 
     @Test
-    void testEncoding() {
+    void checkLastLineInFileContainsLatinEncodedString() {
+        String encodedString = "site.about=Latin1 encoded file äöü"
         String f = "${TestUtils.getTestResourcesAsSourceFolder("/fixtureLatin1")}/jbake.properties"
-        List<String> content = new File(f).getText("ISO-8859-1").split(/\n/)
 
-        Assertions.assertTrue(content[-1].contains("Latin1 encoded file äöü"))
+        List<String> content = new File(f).getText("ISO-8859-1").split(/\n/)
+        Assertions.assertEquals(encodedString, content[-1])
+
+        List<String> contentUtf8 = new File(f).getText("UTF-8").split(/\n/)
+        Assertions.assertNotEquals(encodedString, contentUtf8[-1])
     }
 }
